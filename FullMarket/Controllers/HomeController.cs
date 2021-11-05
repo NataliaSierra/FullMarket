0f036b4c63a73ccccb0804e64e3a09ac4c6f32cf
nataliasierra0404@gmail.com <nataliasierra0404@gmail.com>
2021-11-05 17:43:52 -0500

    First commit NS

--- FullMarket/Controllers/HomeController.cs
+++ FullMarket/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace FullMarket.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Intercambio()
         {
             return View();
         }
